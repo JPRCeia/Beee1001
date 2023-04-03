a68457c10cab602f1d9e561b8fb875490e06cac9
Joao Ceia Ramos <joaopramos076@gmail.com>
2023-04-03 15:25:31 -0300

    bee1001

--- src/App.java
+++ src/App.java
@@ -1,9 +1,22 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int a = 10;
-        int b = 9;
-        int x = a+b;
-
-        System.out.println(x);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class App {   
+    public static void main(String[] args) throws IOException {
+        
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
+        
+        int A, B, X;
+        
+        A = Integer.parseInt(in.readLine());
+        B = Integer.parseInt(in.readLine());
+        
+        X = A + B;
+        
+        System.out.printf("X = %d\n", X);
+        
     }
+    
 }
+
